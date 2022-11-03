bd46d923a6fc6bb518b673636418717785dedb27
Erran7e <s2errante@gmail.com>
2022-11-03 17:38:47 -0400

    lol

--- application.py
+++ application.py
@@ -36,21 +36,18 @@ def get_db():
         db.close()
 
 
-@app.get('/retrieve_all_task_details')
+@app.get('/retrieve_all_task_details', response_model=List[schema.Task_Schema])
 def retrieve_all_task_details(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     task = crud.get_tasks(db=db, skip=skip, limit=limit)
     return task
 
 
-@app.post('/add_new_task')
+@app.post('/add_new_task', response_model=schema.Task_Schema)
 def add_new_task(task: schema.Task_Schema, db: Session = Depends(get_db)):
-    task_id = crud.get_task_by_task_id(db=db, task_id=task.id)
-    if task_id:
-        raise HTTPException(status_code=400, detail=f"task id {task.id} already exist in database: {task_id}")
     return crud.add_task_details_to_db(db=db, task=task)
 
 
-@app.delete('/delete_task_by_id/{id}')
+@app.delete('/delete_task_by_id')
 def delete_task_by_id(sl_id: int, db: Session = Depends(get_db)):
     details = crud.get_task_by_id(db=db, sl_id=sl_id)
     if not details:
@@ -63,7 +60,7 @@ def delete_task_by_id(sl_id: int, db: Session = Depends(get_db)):
     return {"delete status": "success"}
 
 
-@app.put('/update_task_details/{id}')
+@app.put('/update_task_details', response_model=schema.Task_Schema)
 def update_task_details(sl_id: int, update_param: schema.Task_Schema, db: Session = Depends(get_db)):
     details = crud.get_task_by_id(db=db, sl_id=sl_id)
     if not details:
